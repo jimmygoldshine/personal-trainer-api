4d52d57801e22f029db36a84bf0ce598baaf30b6
James <jamesdix@cwgsy.net>
2017-05-22 19:42:50 +0100

    seeded production db

--- Gemfile
+++ Gemfile
@@ -33,5 +33,10 @@ group :development do
   gem 'spring-watcher-listen', '~> 2.0.0'
 end
 
+group :production do
+  gem 'factory_girl_rails'
+  gem 'faker'
+end
+
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
 require 'factory_girl_rails'
 
-FactoryGirl.create_list(:workout,10)
 
-(1..10).each do |id|
-  FactoryGirl.create_list(:exercise, 3, workout_id: id)
+case Rails.env
+when "development"
+  FactoryGirl.create_list(:workout,10)
+  (1..10).each do |id|
+    FactoryGirl.create_list(:exercise, 3, workout_id: id)
+  end
+when "production"
+   FactoryGirl.create(:workout)
+   FactoryGirl.create(:exercise, workout_id: 1)
+   FactoryGirl.create(:set_of, weight: 1000, reps: 50, exercise_id: 1)
 end
