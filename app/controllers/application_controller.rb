47a5621192793688c391c86022320b34960d8b51
James <jamesdix@cwgsy.net>
2017-05-25 09:55:21 +0100

    ssl change to production env

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,4 @@ class ApplicationController < ActionController::API
 
   include Response
 
-  before_filter :expire_hsts
-
-  private
-
-  def expire_hsts
-    response.headers["Strict-Transport-Security"] = 'max-age=0'
-  end
-
 end
